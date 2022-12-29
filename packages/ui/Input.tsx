b4f546582b7cb3d74650c30581d8e566de0a7478
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-12-29 16:56:17 +0700

    change input layout

--- packages/ui/Input.tsx
+++ packages/ui/Input.tsx
@@ -3,5 +3,5 @@ import * as React from 'react'
 interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
 
 export const Input = (props: InputProps) => {
-  return <input {...props} style={{ color: 'red' }} />
+  return <input {...props} style={{ color: 'red', fontSize: '0.9em' }} />
 }
