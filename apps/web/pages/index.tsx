e385c6214fbe34ec93fb534f9b61fd44b4f366e2
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-12-29 15:51:21 +0700

    change web layout

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from 'ui'
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Hello Web</h1>
       <Button />
     </div>
-  );
+  )
 }
