5ec7fcb39c9b4a5ec041eb85bc16ba658bddc7f7
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2023-03-27 21:22:10 +0700

    add version number in response

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,12 @@
 import { Button } from 'ui'
 
+import pk from '../package.json'
+
 export default function Web() {
   return (
     <div>
       <h1>Hello Web developer</h1>
+      <p>Version {pk.version}</p>
       <Button />
     </div>
   )
